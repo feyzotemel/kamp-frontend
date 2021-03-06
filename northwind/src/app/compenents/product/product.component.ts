9f0fd0995ad2386bd4b35b752c333c44ea5d9231
feyzotemel <feyzullah.temel@hotmail.com>
2021-03-07 01:51:52 +0200

    frontend ilk proje

---
+++ northwind/src/app/compenents/product/product.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  product1:any={productId:1, productName:'bardak', categoryId:1, unitPrice:5}
+  product2:any={productId:2, productName:'bardak2', categoryId:1, unitPrice:5}
+  product3:any={productId:3, productName:'bardak3', categoryId:1, unitPrice:5}
+  product4:any={productId:4, productName:'bardak4', categoryId:1, unitPrice:5}
+  product5:any={productId:5, productName:'bardak5', categoryId:1, unitPrice:5}
+  products=[
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
